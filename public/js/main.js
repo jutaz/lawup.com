b94b8035356c6183cfb1e0293f5274734660867d
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-09-26 19:17:28 +0300

    add sticky navbar!

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-    jQuery( '.parallax-layer' ).parallax({});
+    $(".navbar").pinned({
+        bounds: 25, // when to become sticky
+        scrolling: 0, // position during scroll
+        mobile: false // should support mobile 
+    }, function () {
+        $(".navbar").css('width', '100%');
+        $(".navbar").css('margin-left', '0px');
+        $(".navbar").css('margin-right', '0px');
+    }, function () {
+        // UNPINNED
+    });
 });
